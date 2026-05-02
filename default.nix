27f7ee8d32bc10134bf89e5e5c287d7625a1f5d4
Rahul Rameshbabu <sergeantsagara@protonmail.com>
2026-05-02 15:54:50 -0700

    Add support for the production driver branch
    The production branch can sometimes be ahead of the new feature branch.
    
    Signed-off-by: Rahul Rameshbabu <sergeantsagara@protonmail.com>

--- default.nix
+++ default.nix
@@ -29,6 +29,17 @@ rec {
   # Branch/Maturity data - http://people.freedesktop.org/~aplattner/nvidia-versions.txt
 
   # Policy: use the highest stable version as the default (on our master).
+  latest = selectHighestVersion production new_feature;
+
+  bleeding_edge = selectHighestVersion latest beta;
+
+  production = generic {
+    version = "595.71.05";
+    sha256_64bit = "sha256-NiA7iWC35JyKQva6H1hjzeNKBek9KyS3mK8G3YRva4I=";
+    sha256_aarch64 = "sha256-XzKloS00dFKTd4ATWkTIhm9eG/OzR/Sim6MboNZWPu8=";
+    inherit kernel;
+  };
+
   new_feature = generic {
     version = "555.58.02";
     sha256_64bit = "sha256-xctt4TPRlOJ6r5S54h5W6PT6/3Zy2R4ASNFPu8TSHKM=";
