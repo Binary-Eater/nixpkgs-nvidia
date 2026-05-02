4e6127a0ccbd6a80927ebe4456d554be67cc558a
Rahul Rameshbabu <sergeantsagara@protonmail.com>
2026-05-02 15:48:39 -0700

    Use inherit keyword instead of 'kernel = kernel' pattern
    Improve code pattern in default.nix.
    
    Signed-off-by: Rahul Rameshbabu <sergeantsagara@protonmail.com>

--- default.nix
+++ default.nix
@@ -33,13 +33,13 @@ rec {
     version = "555.58.02";
     sha256_64bit = "sha256-xctt4TPRlOJ6r5S54h5W6PT6/3Zy2R4ASNFPu8TSHKM=";
     sha256_aarch64 = "sha256-wb20isMrRg8PeQBU96lWJzBMkjfySAUaqt4EgZnhyF8=";
-    kernel = kernel;
+    inherit kernel;
   };
 
   beta = generic {
     version = "560.28.03";
     sha256_64bit = "sha256-martv18vngYBJw1IFUCAaYr+uc65KtlHAMdLMdtQJ+Y=";
     sha256_aarch64 = "sha256-+u0ZolZcZoej4nqPGmZn5qpyynLvu2QSm9Rd3wLdDmM=";
-    kernel = kernel;
+    inherit kernel;
   };
 }
